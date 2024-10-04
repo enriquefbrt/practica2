8e0aca9d380ffb3927943a2bdda072fa083b78e6
enriquefbrt <enriquefbrt@gmail.com>
2024-10-04 14:11:35 +0200

    added Scooter class and controlled the problems caused by null vehicle plate

---
+++ practica2/Scooter.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice1
+{
+    class Scooter : Vehicle
+    {
+        public static string typeOfVehicle = "Scooter";
+
+        public Scooter() : base(typeOfVehicle, null)
+        {
+            SetSpeed(25.0f);
+        }
+
+        public void StartRide()
+        {
+            Console.WriteLine(WriteMessage("starts riding."));
+        }
+
+        public void StopRide()
+        {
+            Console.WriteLine(WriteMessage("stopped riding."));
+        }
+    }
+}
